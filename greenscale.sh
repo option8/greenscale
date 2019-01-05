b528b262a0e39c1a3738c374647504354a046db6
Charles Mangin <option8@option8.com>
2019-01-05 13:11:30 -0500

    shell script to convert .GIF to .gray
    requires ImageMagick

---
+++ greenscale.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+convert animation.gif frame%05d.gif
+
+convert frame*.gif -compress none -scale 56x100%  -scale 40x48\! -colorspace Gray -colors 16 -depth 8 -auto-level -evaluate divide 17 %d.gray
+
+for file in *.gray
+do
+	cat "$file" | python -u ../greenscale.py >> output.bin
+done
